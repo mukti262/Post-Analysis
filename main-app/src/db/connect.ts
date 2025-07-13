f4e2009312a07d5662e9c9bd524e5045d35803f9
mukti262 <muktibhansali90445@gmail.com>
2025-07-13 13:18:48 +0530

    Added MySQL port and deploy config

--- main-app/src/db/connect.ts
+++ main-app/src/db/connect.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DB,
